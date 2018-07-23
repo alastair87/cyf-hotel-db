6359354f3b1998b49435638714206b603ab9133e
Alastair Cooper <alastair.cooper@outlook.com>
2018-07-23 17:26:13 +0100

    Added schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -22,7 +22,8 @@ CREATE TABLE invoices (
     total             INTEGER,
     surcharges        INTEGER,
     invoice_date_time DATETIME,
-    paid              BOOLEAN  DEFAULT false
+    paid              BOOLEAN  DEFAULT false,
+    FOREIGN KEY(reservation_id) REFERENCES reservations(id)
 );
 
 DROP TABLE IF EXISTS room_types;
@@ -30,8 +31,8 @@ DROP TABLE IF EXISTS room_types;
 CREATE TABLE room_types (
     id        INTEGER       PRIMARY KEY AUTOINCREMENT,
     type_name      VARCHAR (50),
-    original_price INTEGER,
-    current_price  INT
+    original_price NUMBER,
+    current_price  NUMBER
 );
 
 DROP TABLE IF EXISTS rooms;
@@ -39,9 +40,33 @@ DROP TABLE IF EXISTS rooms;
 CREATE TABLE rooms (
     id        INTEGER       PRIMARY KEY AUTOINCREMENT,
     room_type_id INTEGER,
-    sea_view     BOOLEAN
+    sea_view     BOOLEAN,
+    FOREIGN KEY(room_type_id) REFERENCES room_types(id)
 );
 
-INSERT INTO customers (title, firstname, surname, email) VALUES ('Mr.', 'Laurie', 'Ainley', 'laurie@ainley.com');
-INSERT INTO customers (title, firstname, surname, email) VALUES ('Mr.', 'Donald', 'Trump', 'don@twitter.com');
-INSERT INTO customers (title, firstname, surname, email) VALUES ('Ms.', 'Beyonce', 'Knowles', 'beyonce@queen.gov');
+DROP TABLE IF EXISTS reservations;
+
+CREATE TABLE reservations (
+    id INTEGER  PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER,
+    room_id INTEGER,
+    check_in_date DATETIME,
+    check_out_date DATETIME,
+    room_price NUMBER,
+    FOREIGN KEY(customer_id) REFERENCES customers(id),
+    FOREIGN KEY(room_id) REFERENCES rooms(id)
+);
+
+DROP TABLE IF EXISTS  reviews;
+
+CREATE TABLE reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER,
+    rating INTEGER,
+    comment VARCHAR(200),
+    review_date DATETIME
+);
+
+-- INSERT INTO customers (title, firstname, surname, email) VALUES ('Mr.', 'Laurie', 'Ainley', 'laurie@ainley.com');
+-- INSERT INTO customers (title, firstname, surname, email) VALUES ('Mr.', 'Donald', 'Trump', 'don@twitter.com');
+-- INSERT INTO customers (title, firstname, surname, email) VALUES ('Ms.', 'Beyonce', 'Knowles', 'beyonce@queen.gov');
